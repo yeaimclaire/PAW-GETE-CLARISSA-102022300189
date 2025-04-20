8175ef4deac691025a75b50ce14feb1b03498377
Clarissa Wemona Wibawaningrum <claireeesaa@gmail.com>
2025-04-20 23:40:56 +0700

    Tugas TP Modul 3

---
+++ TPMODUL3_CLARISSA/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
+// ==================1==================
+// Tambahkan route GET ke /dashboard yang memanggil method index() dari DashboardController
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::get('/', function () {
+    return redirect('/dashboard');
+});
