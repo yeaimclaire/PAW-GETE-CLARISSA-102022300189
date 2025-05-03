9bbba9aa0c9d6f76487d314ba1860963665b5fab
Clarissa Wemona Wibawaningrum <claireeesaa@gmail.com>
2025-05-03 08:44:53 +0700

    Pengerjaan Jurnal 4

---
+++ JURNALMODUL4_CLARISSA/app/Http/Middleware/Authenticate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class Authenticate
+{
+    public function handle($request, Closure $next)
+    {
+        // Tambahkan pengecualian untuk rute login, register, dan halaman utama
+        if ($request->routeIs('login') || $request->routeIs('register') || $request->is('/')) {
+            return $next($request);
+        }
+
+        // Jika belum login, redirect ke login dengan flash message
+        if (!Auth::check()) {
+            return redirect()->route('login')->with('error', 'Silakan login untuk melanjutkan!');
+        }
+
+        return $next($request);
+    }
+}
