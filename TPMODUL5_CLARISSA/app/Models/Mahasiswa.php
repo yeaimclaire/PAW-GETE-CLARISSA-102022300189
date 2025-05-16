6df3952cd2959c8d55790cc80d4c1a0f011f7f07
Clarissa Wemona Wibawaningrum <claireeesaa@gmail.com>
2025-05-17 03:53:55 +0700

    Pengerjaan TP Modul 5

---
+++ TPMODUL5_CLARISSA/app/Models/Mahasiswa.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    protected $table = 'mahasiswa';
+
+    protected $fillable = [
+        // TODO: Tentukan kolom yang boleh diisi (nama, nim, jurusan, fakultas, foto_profil)
+        'nama',
+        'nim',
+        'jurusan',
+        'fakultas',
+        'foto_profil',
+    ];
+
+    protected function fotoProfil(): Attribute
+    {
+        // TODO: Lengkapi return dari method ini sehingga menghasilkan url dan lokasi foto
+        return Attribute::make(
+            // isi disini
+            get: fn ($value) => $value
+                ? asset('storage/foto_profil/' . $value)
+                : asset('images/default-profile.png'),
+        );
+    }
+}
